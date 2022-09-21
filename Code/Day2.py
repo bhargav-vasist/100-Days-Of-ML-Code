af0a60554531d40e8aa0ff5d31c0e347338555f5
bhargav-vasist <bhargavclick@gmail.com>
2022-09-21 10:35:28 -0400

    Plotted regression for brain data

--- Code/Day2.py
+++ Code/Day2.py
@@ -1,3 +1,4 @@
+# %%
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
@@ -6,19 +7,35 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 dataset = pd.read_csv('../datasets/linear-regression/data.csv')
-X = dataset.iloc[:, : 1].values
-Y = dataset.iloc[:, 1].values
+X = dataset.iloc[:, 2].values
+Y = dataset.iloc[:, 3].values
 
 X_train, X_test, Y_train, Y_test = train_test_split(
     X, Y, test_size=1/4, random_state=0)
 
+X_train = X_train.reshape(-1, 1)
+Y_train = Y_train.reshape(-1, 1)
+X_test = X_test.reshape(-1, 1)
+Y_test = Y_test.reshape(-1, 1)
+# %%
 regressor = LinearRegression()
 regressor = regressor.fit(X_train, Y_train)
 
 Y_pred = regressor.predict(X_test)
 
+# %%
 plt.scatter(X_train, Y_train, color='red')
+plt.title = "Predict Brain Mass for Brain Head Size"
+plt.xlabel = "Brain head size in Cm^3"
+plt.ylabel = "Brain weight in grams"
 plt.plot(X_train, regressor.predict(X_train), color='blue')
 
+
+# %%
 plt.scatter(X_test, Y_test, color='red')
+plt.title = "Predict Brain Mass for Brain Head Size"
+plt.xlabel = "Brain head size in Cm^3"
+plt.ylabel = "Brain weight in grams"
 plt.plot(X_test, regressor.predict(X_test), color='blue')
+
+# %%
